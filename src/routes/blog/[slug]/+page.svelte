45a0eafe15f75cc6e4693ee856b26c90d326c6b7
Ali G <ali@basicavisual.io>
2024-08-20 17:47:55 -0600

    removed console.log and account for not authors

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -15,11 +15,13 @@
 
   <div class="container m-auto p-3 prose">
     <h1 class="text-center  font-bold">{post.title}</h1>
-    <p class="text-center">Por 
-      {#each post.authors as author, i }
-      <span>{author.authors_id.name || ''}{#if i < (post.authors.length-1)}, {/if}</span>
-      {/each}
-    </p>
+    {#if post.authors}
+      <p class="text-center">Por 
+        {#each post.authors as author, i }
+        <span>{author.authors_id.name || ''}{#if i < (post.authors.length-1)}, {/if}</span>
+        {/each}
+      </p>
+    {/if}
     <p class="text-center text-sm text-gray-400">{new Date(post.date_created).toLocaleString('es-MX')}</p>
   </div>
   <img src={`https://content.codeandomexico.org/assets/${post.post_image}`} alt="Imagen de blog" class="w-full max-h-[450px] object-cover" />

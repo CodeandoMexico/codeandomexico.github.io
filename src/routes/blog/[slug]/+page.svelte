0592f09237dbd80441d5ff0f4ebe21d053b317e5
Ali G <ali@basicavisual.io>
2024-08-20 17:27:08 -0600

    fixed margin

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -11,16 +11,20 @@
   <meta property="og:image" content={`https://content.codeandomexico.org/assets/${post.post_image}`} />
 </svelte:head>
 
-<section class="container m-auto p-3 prose">
-  <h1 class="text-center  font-bold">{post.title}</h1>
-  <p class="text-center">Por 
-    {#each post.authors as author, i }
-    <span>{author.authors_id.name || ''}{#if i < (post.authors.length-1)}, {/if}</span>
-    {/each}
-  </p>
-  <p class="text-center text-sm text-gray-400">{new Date(post.date_created).toLocaleString('es-MX')}</p>
-</section>
-<img src={`https://content.codeandomexico.org/assets/${post.post_image}`} alt="Imagen de blog" class="w-full max-h-[450px] object-cover" />
-<section class="container m-auto p-3 prose prose-blockquote:text-2xl prose-blockquote:border-green-400">
+<section class="my-10">
+
+  <div class="container m-auto p-3 prose">
+    <h1 class="text-center  font-bold">{post.title}</h1>
+    <p class="text-center">Por 
+      {#each post.authors as author, i }
+      <span>{author.authors_id.name || ''}{#if i < (post.authors.length-1)}, {/if}</span>
+      {/each}
+    </p>
+    <p class="text-center text-sm text-gray-400">{new Date(post.date_created).toLocaleString('es-MX')}</p>
+  </div>
+  <img src={`https://content.codeandomexico.org/assets/${post.post_image}`} alt="Imagen de blog" class="w-full max-h-[450px] object-cover" />
+  <div class="container m-auto p-3 prose prose-blockquote:text-2xl prose-blockquote:border-green-400">
   <p>{@html post.content}</p>
+</div>
+
 </section>

526b404e2ec5b2a7c5eb0391747c9faea430518a
flkt crnpio <flkt.crnpio@gmail.com>
2026-07-03 10:39:53 -0600

    cambiando header h1 a h2 para organizar jerarquia de texto

--- src/components/TitleDescription.svelte
+++ src/components/TitleDescription.svelte
@@ -2,8 +2,8 @@
 	let { title, description = '' } = $props();
 </script>
 
-<div class="">
-	<h1 class="text-5xl font-bold">{title}</h1>
+<div>
+	<h2 class="text-5xl font-bold">{title}</h2>
 	{#if description}
 	<p class="py-6 text-2xl">
 		{description}

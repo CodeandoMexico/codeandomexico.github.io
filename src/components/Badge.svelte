43c223c0c78e221ab54fb3597d84a118a35ead20
flkt crnpio <flkt.crnpio@gmail.com>
2026-07-06 11:55:44 -0600

    ajustando padding

--- src/components/Badge.svelte
+++ src/components/Badge.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <span>
-	<a href="/blog/tag/{text}" class="rounded-full px-3 py-2 text-xs bg-cmxblack text-white whitespace-nowrap hover:bg-cmxgreen hover:text-black focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxgreen focus:bg-cmxgreen focus:text-black">
+	<a href="/blog/tag/{text}" class="rounded-full px-3 py-1 text-xs bg-cmxblack text-white whitespace-nowrap hover:bg-cmxgreen hover:text-black focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxgreen focus:bg-cmxgreen focus:text-black">
 		<span class="sr-only">Buscar etiqueta:</span>
 		{text}
 	</a>

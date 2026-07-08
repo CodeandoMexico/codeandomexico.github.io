22c7af2ff76ad475f773119e318be3383ea4e09d
flkt crnpio <flkt.crnpio@gmail.com>
2026-07-08 10:17:50 -0600

    quitando alt text redundante. agregando titulo. agregando informacion de enlace externo

--- src/components/Cards/ProjectCard.svelte
+++ src/components/Cards/ProjectCard.svelte
@@ -1,6 +1,7 @@
 <script>
-	import Badge from "../Badge.svelte";
+	import Badge from "@/components/Badge.svelte";
 	import CmsImage from "@/components/CmsImage.svelte";
+  import ExternalLinkIcon from "@/components/Icon/ExternalLink.svelte";
   /**
    * @typedef {Object} Props
    * @property {any} title
@@ -26,11 +27,11 @@
 
 
 <div class="Card w-full min-h-[519px] flex-col justify-start items-start inline-flex">
-  <CmsImage {assetId} alt={title} class="Image4 object-cover h-[200px] w-full" />
+  <CmsImage {assetId} alt={''} class="Image4 object-cover h-[200px] w-full" />
   <div class="Frame5 self-stretch min-h-[269px] h-full flex-col justify-start pt-3 items-start gap-4 flex">
     <div class="Heading self-stretch min-h-[84px] flex-col justify-start items-start gap-8 flex">
       <div class="Heading self-stretch text-black text-[31.25px] font-bold leading-10">
-        {title}
+        <h3>{title}</h3>
       </div>
     </div>
     <div class="Descripion self-stretch justify-start items-start inline-flex">
@@ -42,20 +43,23 @@
       <p>{maker}</p>
     </div>
     <div class="flex flex-wrap gap-2">
-
       {#each tags as tag}
         <Badge text={tag}/>
       {/each}
     </div>
     <div class="flex gap-3 my-2">
       {#if website}
-      <a href={website} target="_blank" class="border-2 border-black uppercase font-bold bg-black text-white px-5 py-2 hover:bg-gray-700 transition duration-300 ease-in-out">
+      <a href={website} target="_blank" class="border-2 border-black uppercase font-bold bg-black text-white px-5 py-2 hover:bg-gray-700 transition duration-300 ease-in-out focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxgreen focus:border-cmxgreen">
         Sitio web
+        <span class="sr-only"> de {title}.</span>
+        <ExternalLinkIcon />
       </a>
       {/if}
       {#if repository}
-      <a href={repository} target="_blank" class="border-2 border-black uppercase font-bold bg-white text-black px-5 py-2 hover:bg-gray-200 transition duration-300 ease-in-out">
+      <a href={repository} target="_blank" class="border-2 border-black uppercase font-bold bg-white text-black px-5 py-2 hover:bg-gray-200 transition duration-300 ease-in-out focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxgreen focus:border-cmxgreen">
         Repositorio
+        <span class="sr-only">de {title}.</span>
+				<ExternalLinkIcon />
       </a>
       {/if}
     </div>

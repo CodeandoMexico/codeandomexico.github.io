ab0e6a734094d2d65f43d6f739dc89f10ce7eb40
flkt crnpio <flkt.crnpio@gmail.com>
2026-07-09 12:36:55 -0600

    agregando estilos de estado a la etiqueta de enlace. agregando icono de enlace externo

--- src/components/Cards/CommunityCard.svelte
+++ src/components/Cards/CommunityCard.svelte
@@ -1,13 +1,13 @@
 <script>
+  import ExternalLinkIcon from "@/components/Icon/ExternalLink.svelte";
   let { text, route } = $props();
 </script>
 
 <div class="p-5 w-full md:w-1/3">
-  <a href="{route}" target="_blank">
-    <div class="border border-cmxyellow p-5 h-[150px] flex justify-between flex-col hover:bg-zinc-100 transition-colors duration-200">
-      <h3 class="text-lg font-bold">{text}</h3>
-      <img src="/flecha-amarilla.png" class="inline h-[20px] w-[20px] 
-      self-end" alt="flecha amarilla">
-    </div>
+  <a href="{route}" target="_blank" class="border border-cmxyellow p-5 h-[150px] flex justify-between flex-col hover:bg-zinc-100 transition-colors duration-200 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxyellow focus:bg-zinc-100">
+    <h3 class="text-lg font-bold">{text}</h3>
+    <span class="inline h-[20px] w-[20px] self-end text-cmxyellow">
+      <ExternalLinkIcon  />
+    </span>
   </a>
 </div>

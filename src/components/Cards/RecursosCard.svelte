709d7002b6519616c63c263a3d6e378d1f5376b8
flkt crnpio <flkt.crnpio@gmail.com>
2026-07-11 10:20:01 -0600

    agregando estilos de enfoque. quitando logica de layout, se movio al contenedor en la vista. homologando icono de enlace externo

--- src/components/Cards/RecursosCard.svelte
+++ src/components/Cards/RecursosCard.svelte
@@ -1,19 +1,27 @@
 <script>
-  let { title, url, authors = undefined } = $props();
+  import ExternalLinkIcon from "@/components/Icon/ExternalLink.svelte";
+  let { title='', subtitle='', url, authors = undefined } = $props();
 </script>
 
-<a href={url} target="_blank" rel="noopener noreferrer" class=" w-full md:w-1/3 md:pr-8 mb-5">
-  <div class="bg-gray-100 hover:bg-gray-200 h-[150px] p-5 flex justify-between">
-    <div>
-      <h4 class="text-xl font-bold">{title}</h4>
-      {#if authors}
-      <p>
-        {#each authors as author, i }
-            <span>{author.authors_id.name || ''}{#if i < (authors.length-1)}, {/if}</span>
-        {/each}
-      </p>
-      {/if }
-    </div>
-    <img src="/flecha-verde.png" alt="Ir al recurso" class="inline h-[20px] w-[20px] self-end">
+<a href={url} target="_blank" rel="noopener noreferrer" 
+  class="bg-zinc-100 hover:bg-zinc-200 h-[150px] p-5 flex justify-between focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-cmxred"
+>
+  <div>
+    {#if title}
+      <h3 class="text-xl font-bold">{title}</h3>
+    {/if}
+    {#if subtitle}
+      <h4 class="text-xl font-bold">{subtitle}</h4>
+    {/if}
+    {#if authors}
+    <p>
+      {#each authors as author, i }
+        <span>{author.authors_id.name || ''}{#if i < (authors.length-1)}, {/if}</span>
+      {/each}
+    </p>
+    {/if}
   </div>
+  <span class="inline h-[20px] w-[20px] self-end text-cmxred">
+    <ExternalLinkIcon  />
+  </span>
 </a>
